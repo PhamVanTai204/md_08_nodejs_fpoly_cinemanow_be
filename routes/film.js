263d0e024ca2f0636c71cb2be9203db0842bd6a7
Sandapchai <nguyensan77@gmail.com>
2025-02-18 14:43:08 +0700

    get-by-id

--- routes/film.js
+++ routes/film.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 const uc = require('../controllers/film.controller')
 
+//Lấy hết phim
 router.get('/getfilm', uc.getFilm)
+//Lấy phim theo id 
+router.get('/getfilm/:id', uc.getFilmId)
 router.post('/addfilm', uc.addFilm)
 router.put('/editfilm/:id', uc.updateFilm)
 router.delete('/deletefilm/:id', uc.deleteFilm)
