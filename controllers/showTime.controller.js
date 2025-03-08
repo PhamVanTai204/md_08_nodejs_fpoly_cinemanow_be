8eb9a3e95297980f5e17917d04e6cd19db85a289
PhamVanTai204 <taipv204@gmail.com>
2025-03-08 13:17:48 +0700

    chức năng getbyid

--- controllers/showTime.controller.js
+++ controllers/showTime.controller.js
@@ -13,3 +13,17 @@ exports.getShowTimes = async (req, res) => {
     }
 };
 
+
+// Lấy suất chiếu theo ID
+exports.getShowTimeById = async (req, res) => {
+    try {
+        const showTime = await ShowTime.findById(req.params.id).populate('movieId');
+        if (!showTime) {
+            return res.status(404).json(createResponse(404, 'Không tìm thấy suất chiếu', null));
+        }
+        res.status(200).json(createResponse(200, null, showTime));
+    } catch (error) {
+        res.status(500).json(createResponse(500, 'Lỗi khi lấy suất chiếu', error.message));
+    }
+};
+
--- routes/showTime.js
+++ routes/showTime.js
@@ -5,5 +5,7 @@ const showTimeController = require('../controllers/showTime.controller');
 // Lấy danh sách suất chiếu
 router.get('/getAll', showTimeController.getShowTimes);
 
+// Lấy suất chiếu theo ID
+router.get('/getById/:id', showTimeController.getShowTimeById);
 
 module.exports = router;
