27dfce954af765a25c70cdc8fb391e235bc4dd0f
PhamVanTai204 <taipv204@gmail.com>
2025-03-17 16:10:50 +0700

    update lỗi cros cho web

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Chỉ khai báo một lần!
+
 // Load environment variables
 dotenv.config();
 
@@ -20,7 +22,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
