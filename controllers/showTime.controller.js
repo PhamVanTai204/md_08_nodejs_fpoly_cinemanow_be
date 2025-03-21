5bf51da020840af676e1519d53b7b7258a245eae
PhamVanTai204 <taipv204@gmail.com>
2025-03-21 14:31:55 +0700

    thêm hiển thị cả film trong show time

--- controllers/showTime.controller.js
+++ controllers/showTime.controller.js
@@ -6,7 +6,7 @@ const createResponse = require('../utils/responseHelper');
 // Lấy danh sách suất chiếu
 exports.getShowTimes = async (req, res) => {
     try {
-        const showTimes = await ShowTime.find(); // Lấy thông tin phim kèm theo
+        const showTimes = await ShowTime.find().populate('movie_id'); // Lấy thông tin phim kèm theo
         res.status(200).json(createResponse(200, null, showTimes));
     } catch (error) {
         res.status(500).json(createResponse(500, 'Lỗi khi lấy danh sách suất chiếu', error.message));
