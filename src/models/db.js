a599e76d09d76c0b10e2bb5b4fb2766a92be07eb
nguyenquangtung004 <nguyenquangtung04.cv@gmail.com>
2025-05-14 16:43:59 +0700

    TODO: Chỉnh sửa sài đường dẫn local db trong máy

--- src/models/db.js
+++ src/models/db.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config(); // su dung thu vien doc file env:   npm install dotenv --save
 const DB_NAME = process.env.DB_NAME;
-
-mongoose.connect(
-        `mongodb+srv://sanndph32936:sandapchai12@cluster0.cstvl.mongodb.net/Learning?retryWrites=true&w=majority&appName=Cluster0`
-).then(
+// `mongodb+srv://sanndph32936:sandapchai12@cluster0.cstvl.mongodb.net/Learning?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.connect(`mongodb://127.0.0.1:27017/cinema`).then(
         () => {
                 console.log(DB_NAME);
 
